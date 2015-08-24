ff0d3f2ebabd2faf1d730dd428a1d7a9ba9d638a
Jiaqi Zhu <zhujiaqi.apply@gmail.com>
2015-08-24 17:01:20 -0400

    Update weather.py

--- Elutions/weather.py
+++ Elutions/weather.py
@@ -13,10 +13,10 @@ import re
 import time
 import pandas as pd
 
-base_url1 = 'http://api.wunderground.com/api/api-key/history_'
-base_url2 = 'http://api.wunderground.com/api/api-key/history_'
-base_url3 = 'http://api.wunderground.com/api/api-key/history_'
-start_date = datetime.date(2013,06,29);
+base_url1 = 'http://api.wunderground.com/api/165967bed6d1af1b/history_'
+base_url2 = 'http://api.wunderground.com/api/fdd96b9124a76f1e/history_'
+base_url3 = 'http://api.wunderground.com/api/ae63a141e2e05630/history_'
+start_date = datetime.date(2013,8,10);
 locations = ['Bristol','Northolt','Birmingham','Exeter','Brize Norton','Biggin Hill','Cranfield','Glasgow','Leeds And Bradford','Farnborough','Manchester','Heathrow','Edinburgh Airport']
 #locations = ['Bristol','Northolt']
 bases = [base_url1, base_url2, base_url3]
@@ -58,4 +58,4 @@ for loc in locations:
     print result[loc]
 
 df = pd.DataFrame(result).T[dates]
-df.to_csv('output.csv')
+df.to_csv('13 site Aug 10-16.csv')
